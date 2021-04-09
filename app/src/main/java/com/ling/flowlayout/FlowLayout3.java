bb36dfb35f252e950645297fa8f7f833d0bbee88
hao2.ling <hao2.ling@tcl.com>
2021-04-09 15:33:15 +0800

    第一阶段代码

--- app/src/main/java/com/ling/flowlayout/FlowLayout3.java
+++ app/src/main/java/com/ling/flowlayout/FlowLayout3.java
@@ -6,7 +6,6 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -33,40 +32,71 @@ public class FlowLayout3 extends RelativeLayout {
         init();
     }
 
-
+    /**
+     * 记录所有行的 view 集合
+     */
     ArrayList<ArrayList<View>> alls = new ArrayList();
+    /**
+     * 记录每一行的 view 集合
+     */
     ArrayList<View> lines = new ArrayList();
 
-    LayoutParams ddddddddd = null;
 
     private void init() {
         datas = Utils.getDataas();
 
-
     }
+
+    LayoutParams layoutParams;
 
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
         Log.i("asss", "onMeasure()");
         alls.clear();
         lines.clear();
-        int w = 0, h = 0;
-        int WWW = 1280, HHH = 800;
-        int tempW = 0;
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
-
         int count = getChildCount();
-        if (count==0) return;
+        if (count == 0) {
+            return;
+        }
+
+        int width = 0, height = 0;
+        int pad_top = getPaddingTop();
+        int pad_right = getPaddingRight();
+        int pad_bottom = getPaddingBottom();
+        int pad_left = getPaddingLeft();
+        Log.i("layoutParams", "pad_top == " + pad_top + ")pad_right =" + pad_right);
+
+        int selftWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int selfHeight = MeasureSpec.getSize(heightMeasureSpec);
+
+
+        int modeWidht = MeasureSpec.getMode(widthMeasureSpec);
+        int modeHeight = MeasureSpec.getMode(heightMeasureSpec);
+
         for (int i = 0; i < count; i++) {
-            getChildAt(i).measure(widthMeasureSpec, heightMeasureSpec);
-            LayoutParams params = (LayoutParams) getChildAt(i).getLayoutParams();
-            ddddddddd = params;
-            tempW += getChildAt(i).getMeasuredWidth() + params.leftMargin + params.rightMargin;
-            if (tempW > WWW) {
-                tempW = 0;
-                tempW += getChildAt(i).getMeasuredWidth() + params.leftMargin + params.rightMargin;
+            layoutParams = (LayoutParams) getChildAt(i).getLayoutParams();
+            layoutParams.rightMargin = 20;
+            layoutParams.leftMargin = 20;
+            layoutParams.topMargin = 20;
+            layoutParams.bottomMargin = 20;
+            int childWidthMeasureSpec = getChildMeasureSpec(widthMeasureSpec, pad_left + pad_right, layoutParams.width);
+            int childHeightMeasureSpec = getChildMeasureSpec(heightMeasureSpec, pad_top + pad_bottom, layoutParams.height);
+
+            getChildAt(i).measure(childWidthMeasureSpec, childHeightMeasureSpec);
+
+            int childWidth = getChildAt(i).getMeasuredWidth();
+            int childHeigth = getChildAt(i).getMeasuredWidth();
+
+
+            Log.i("layoutParams", "width == " + layoutParams.width);
+
+
+            width += childWidth + layoutParams.leftMargin + layoutParams.rightMargin;
+            if (width > selftWidth - pad_left - pad_right) {
+                width = 0;
                 alls.add((ArrayList<View>) lines.clone());
                 lines.clear();
             }
@@ -75,17 +105,34 @@ public class FlowLayout3 extends RelativeLayout {
         }
         alls.add((ArrayList<View>) lines.clone());
         if (alls.size() > 1) {
-            w = WWW;
-            h = alls.size() * (getChildAt(0).getMeasuredHeight() + ddddddddd.topMargin + ddddddddd.bottomMargin);
-        } else {
-            w = tempW;
-            h = getChildAt(0).getMeasuredHeight() + ddddddddd.topMargin + ddddddddd.bottomMargin;
-        }
-        Log.i("ddddddd3", "w == " + w + ")( h ==" + h + ")( " + alls.get(0).size());
-        Log.i("ddddddd3", "w == " + w + ")( h ==" + h + ")( " + alls.size());
-        Log.i("ddddddd3", "w == " + w + ")( h ==" + h + ")( " + alls.get(0).get(0).getMeasuredWidth());
+            width = selftWidth;
+            height = alls.size() * (getChildAt(0).getMeasuredHeight()+ pad_bottom + pad_top + layoutParams.topMargin  + layoutParams.bottomMargin) ;
+            Log.i("asss", "alls.size() == " + alls.size() + ")(getChildAt(0).getMeasuredHeight() == " + getChildAt(0).getMeasuredHeight());
 
-        setMeasuredDimension(w, h);
+        } else {
+            width = getChildAt(0).getMeasuredHeight();
+        }
+        switch (modeHeight) {
+            case MeasureSpec.AT_MOST:
+                break;
+            case MeasureSpec.UNSPECIFIED:
+                break;
+            case MeasureSpec.EXACTLY:
+                width = selftWidth;
+                break;
+        }
+        switch (height) {
+            case MeasureSpec.AT_MOST:
+                break;
+            case MeasureSpec.UNSPECIFIED:
+                break;
+            case MeasureSpec.EXACTLY:
+                height = selfHeight;
+                break;
+        }
+        Log.i("asss", "width == " + width + ")(height == " + height);
+
+        setMeasuredDimension(width, height);
 
     }
 
@@ -94,14 +141,17 @@ public class FlowLayout3 extends RelativeLayout {
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         Log.i("asss", "onLayout()");
 
-        if (alls.size()==0) return;
-        int left = 0;
-        int top = 0;
+        if (alls.size() == 0) {
+            return;
+        }
+        int left = getPaddingLeft();
+        int top = getPaddingTop();
         for (int i = 0; i < alls.size(); i++) {
             int mtop = alls.get(0).get(0).getMeasuredHeight();
-            left = 0;
+            left = getPaddingLeft();
             ArrayList<View> datas = alls.get(i);
             for (int j = 0; j < datas.size(); j++) {
+                layoutParams = (LayoutParams) getChildAt(j).getLayoutParams();
                 View view = datas.get(j);
                 int mleft = view.getMeasuredWidth();
 
@@ -109,13 +159,16 @@ public class FlowLayout3 extends RelativeLayout {
                     Log.i("ddddddd", "left == " + alls.get(0).get(0).getMeasuredWidth());
                 }
 
-                view.layout(left + ddddddddd.leftMargin, top + ddddddddd.topMargin, left + mleft + ddddddddd.rightMargin, top + mtop + ddddddddd.bottomMargin);
+                view.layout(left + layoutParams.leftMargin, top + layoutParams.topMargin, left + mleft + layoutParams.rightMargin, top + mtop + layoutParams.bottomMargin);
 
-                left += mleft + ddddddddd.leftMargin + ddddddddd.rightMargin;
+                left += mleft + layoutParams.leftMargin + layoutParams.rightMargin;
 
                 Log.i("ddddddd", "left MMM == " + left);
+                if (j == (datas.size() - 1)) {
+                    top += mtop + layoutParams.topMargin + layoutParams.bottomMargin;
+                }
             }
-            top += mtop + ddddddddd.topMargin + ddddddddd.bottomMargin;
+
 
         }
 
--- app/src/main/java/com/ling/flowlayout/MainActivity.kt
+++ app/src/main/java/com/ling/flowlayout/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.ling.flowlayout
 
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.RelativeLayout
 import android.widget.TextView
@@ -10,44 +10,32 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
 
-    lateinit var  flowlayout3:RelativeLayout
+    lateinit var flowlayout3: RelativeLayout
     lateinit var strs: MutableList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var tess = findViewById<TextView>(R.id.tv_test)
         flowlayout3 = findViewById<FlowLayout3>(R.id.flowlayout3)
-    }
-
-
-    fun dddddddd(view: View) {
-        var tess = findViewById<TextView>(R.id.tv_test)
-        tess.text = "ddddddddddddddddddd"
         addViewsss()
-
     }
+
+
+//    fun dddddddd(view: View) {
+//        var tess = findViewById<TextView>(R.id.tv_test)
+////        tess.text = "ddddddddddddddddddd"
+//
+//
+//    }
 
     private fun addViewsss() {
-        var datas:ArrayList<String> = Utils.getDataas()
-        for (i in datas.indices) {
-            //10sp/20px 大小计算
-            val params = RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT
-            )
-            params.bottomMargin = 20
-            params.topMargin = 20
-            params.leftMargin = 20
-            params.rightMargin = 20
-            params.width = datas.get(i).length * 20
-            params.height = 20
-            val view = TextView(this)
-                        view.setTextColor(getResources().getColor(R.color.white));
-//            view.setTextAppearance(getContext(), R.style.mystyle);
-            view.layoutParams = params
-            Log.i("asss","flowlayout3  = "+flowlayout3)
-            flowlayout3?.addView(view)
+        var datas: ArrayList<String> = Utils.getDataas()
+        datas.forEach { str ->
+            var view = LayoutInflater.from(this).inflate(R.layout.text_view_layout, null)
+            view as TextView
+            view.text = str
+            flowlayout3.addView(view)
+
         }
     }
 
